18d4f9d8a2d004bcd3161c215f75897257f6a9a5
Michael Gicking <mgicking@catalyte.io>
2019-06-18 17:36:13 -0400

    Adding data files and project for multiclass classification example

---
+++ IssueClassification/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IssueClassification
+{
+    class Program
+    {
+        private 
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
