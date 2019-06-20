2dabc30922e4677b9f8c4d5e17064359d4507da6
Michael Gicking <mgicking@catalyte.io>
2019-06-20 11:14:59 -0400

    Adding skeleton for abstracting business logic to service layer. Adding method for extracting/transforming data for model training

--- IssueClassification/IssueClassification/Program.cs
+++ IssueClassification/IssueClassification/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using IssueClassification.IssueClassification.Services;
 using Microsoft.ML;
 
 namespace IssueClassification
@@ -23,9 +24,39 @@ namespace IssueClassification
         private static PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
         #endregion
 
+        #region dependencies
+        // Service that does all processing logic for multiclass classification
+        private readonly GitHubIssueClassificationService _classificationService = new GitHubIssueClassificationService();
+        #endregion
+
         static void Main(string[] args)
         {
-            
+            // Initialize the MLContext with a random(0) seed for repeatable/deterministic results across multiple trainings
+            _mlContext = new MLContext(0);
+
+            // Load the data from text file and map columns to fields in the GitHubIssue class
+            _trainingDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue>(_trainDataPath, hasHeader: true);
+
+            // Extract and transform data and return processing pipeline
+            var pipeline = ProcessData();
+        }
+
+        // Extract features and transform the data into something that can be used by Microsoft.ML to train the model
+        public static IEstimator<ITransformer> ProcessData()
+        {
+            // Transform the "Area" column into numeric key type "Label" column and add as new dataset
+            // Transform text "Title" and "Description" Features into numeric vectors
+            // Combine all feature columns into a new column called "Features"
+            // Cache the DataView to improving performance of iterating over data multiple times
+            var pipeline = _mlContext.Transforms.Conversion.MapValueToKey("Area", "Label")
+                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Title", outputColumnName: "TitleFeaturized"))
+                .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Description", outputColumnName: "DescriptionFeaturized"))
+                .AppendCacheCheckpoint(_mlContext);
+
+            // NOTE: AppendCacheCheckpoint is designed to be used for small/medium datasets to lower
+            // training time and is NOT to be used when handling very large datasets
+
+            return pipeline;
         }
     }
 }
---
+++ IssueClassification/IssueClassification/Services/GitHubIssueClassificationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IssueClassification.IssueClassification.Services
+{
+    public class GitHubIssueClassificationService
+    {
+
+    }
+}
