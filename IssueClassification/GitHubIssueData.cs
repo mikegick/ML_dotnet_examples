b0300cd9120e25cae9feabd5cd408bb43468cca4
Michael Gicking <mgicking@catalyte.io>
2019-06-20 10:09:22 -0400

    Adding data models

--- IssueClassification/GitHubIssueData.cs
+++ IssueClassification/GitHubIssueData.cs
@@ -9,10 +9,13 @@ namespace IssueClassification
     {
         [LoadColumn(0)]
         public string ID { get; set; }
+        // The area of the solution where the GitHub Issue is located, this is what we'll be trying to predict
         [LoadColumn(1)]
         public string Area { get; set; }
+        // GitHub issue title, the first feature used for prediction
         [LoadColumn(2)]
         public string Title { get; set; }
+        // GitHub issue description, the second feature used for prediction
         [LoadColumn(3)]
         public string Description { get; set; }
     }
