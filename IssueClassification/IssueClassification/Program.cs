6ed5b9b348b0ca5b98d20cd3d3271099e2de0858
Michael Gicking <mgicking@catalyte.io>
2019-06-25 09:45:03 -0400

    Building and Training the model for prediction

--- IssueClassification/IssueClassification/Program.cs
+++ IssueClassification/IssueClassification/Program.cs
@@ -22,6 +22,8 @@ namespace IssueClassification
         private static IDataView _trainingDataView;
         // PredictionEngine object used for single predictions
         private static PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+        // ITransformer object that acts as the model
+        private static ITransformer _trainedModel;
         #endregion
 
         #region dependencies
@@ -39,6 +41,9 @@ namespace IssueClassification
 
             // Extract and transform data and return processing pipeline
             var pipeline = ProcessData();
+
+            // Build and train the model
+            var trainingPipeline = BuildAndTrainModel(_trainingDataView, pipeline);
         }
 
         // Extract features and transform the data into something that can be used by Microsoft.ML to train the model
@@ -52,11 +57,46 @@ namespace IssueClassification
                 .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Title", outputColumnName: "TitleFeaturized"))
                 .Append(_mlContext.Transforms.Text.FeaturizeText(inputColumnName: "Description", outputColumnName: "DescriptionFeaturized"))
                 .AppendCacheCheckpoint(_mlContext);
-
             // NOTE: AppendCacheCheckpoint is designed to be used for small/medium datasets to lower
             // training time and is NOT to be used when handling very large datasets
 
             return pipeline;
+        }
+
+        public static IEstimator<ITransformer> BuildAndTrainModel(IDataView trainingDataView, IEstimator<ITransformer> pipeline)
+        {
+            // Since we want to be able to predict the area of a GitHub issue as being in one of many places, we need to
+            // do multiclass classification. We will use the SdcaMaximumEntropy classification training algorithm.
+            // The SdcaMaximumEntropy algorithm will take in the "Label" and "Features" (Title, Description) to learn from
+            // historic data and build a model from it.
+            var trainingPipeline = pipeline.Append(_mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))
+                .Append(_mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+            // Visit https://docs.microsoft.com/en-us/dotnet/api/microsoft.ml.trainers.sdcamaximumentropymulticlasstrainer for
+            // more info on the SdcaMaximumEntropy training algorithm
+
+            // Train the model
+            _trainedModel = trainingPipeline.Fit(trainingDataView);
+
+            // Use the PredictionEngine convenience API to perform a single prediction
+            _predEngine = _mlContext.Model.CreatePredictionEngine<GitHubIssue, IssuePrediction>(_trainedModel);
+
+            // Use the trained model to perfom a prediction using the PredictionEngine
+            // 1) Build a test issue
+            GitHubIssue issue = new GitHubIssue()
+            {
+                Title = "WebSockets communication is slow in my machine",
+                Description = "The Websockets communication used under the covers by SignalR looks like it is going slow in my" +
+                "development environment."
+            };
+
+            // 2) Use the Prediction() function to make a prediction on the test issue
+            var prediction = _predEngine.Predict(issue);
+
+            // 3) Print the results of the prediction to the console
+            Console.WriteLine($"Single Prediction of just-trained model on test instance: {prediction.Area}");
+
+            // Return the trained model to use for evaluation
+            return trainingPipeline;
         }
     }
 }
